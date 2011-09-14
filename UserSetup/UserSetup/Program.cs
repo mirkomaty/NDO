da01726935bdc0feb0d1930dc7ce3aa4925895d3
mirko <mirko@e088091c-91c0-7b4b-9d36-2eafbd79b41f>
2011-09-14 15:46:46 +0000

    * Implemented the new UserSetup logic, which writes a dummy .AddIn file.
    git-svn-id: http://www.netdataobjects.de:81/svn/NDO20/trunk@105 e088091c-91c0-7b4b-9d36-2eafbd79b41f

---
+++ UserSetup/UserSetup/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UserSetup
+{
+	class Program
+	{
+		static readonly string xml = @"<?xml version=""1.0"" encoding=""UTF-16"" standalone=""no""?>
+<Extensibility xmlns=""http://schemas.microsoft.com/AutomationExtensibility"">
+	<Addin>
+		<Assembly>###</Assembly>
+	</Addin>
+</Extensibility>";
+
+		[STAThread]
+		static int Main( string[] args )
+		{
+			string ndoPath = null;
+			if ( args.Length > 0 )
+			{
+				ndoPath = args[0];
+			}
+			else
+			{
+				FolderBrowserDialog fbd = new FolderBrowserDialog();
+				fbd.Description = "Select the NDO installation directory";
+				fbd.SelectedPath = Environment.GetFolderPath( Environment.SpecialFolder.ProgramFiles );
+				DialogResult result = fbd.ShowDialog();
+				if ( result == DialogResult.OK )
+				{
+					ndoPath = fbd.SelectedPath;
+				}
+				else
+				{
+					return -2;
+				}
+			}
+			if ( !Directory.Exists( ndoPath ) )
+			{
+				MessageBox.Show( "NDO installation directory not found at '" + ndoPath +"'");
+				return -1;
+			}
+			ndoPath = Path.Combine( ndoPath, "NDOEnhancer.dll" );
+			string path = Path.Combine( Environment.GetFolderPath( Environment.SpecialFolder.CommonApplicationData ), @"Microsoft\MSEnvShared\AddIns\NDO21.AddIn" );
+			StreamWriter sw = new StreamWriter( path, false, Encoding.Unicode );
+			sw.Write( xml.Replace( "###", ndoPath ) );
+			sw.Close();
+
+			return 0;
+		}
+	}
+}
