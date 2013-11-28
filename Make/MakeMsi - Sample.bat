df756d0f23f7456f2b032791a319b6c859448825
mirko <mirko@e088091c-91c0-7b4b-9d36-2eafbd79b41f>
2013-11-28 15:27:16 +0000

    NDO will be built using the NDOInstaller solution.
    git-svn-id: http://www.netdataobjects.de:81/svn/NDO20/trunk@152 e088091c-91c0-7b4b-9d36-2eafbd79b41f

--- Make/MakeMsi - Sample.bat
+++
@@ -1,3 +0,0 @@
-@"C:\Program Files (x86)\WiX Toolset v3.6\bin\candle.exe" -ext WixNetFxExtension -ext wixUtilExtension -sw1044 -out NDODevEdition21.wixobj NDOEnhancerEnterprise21.xml 
-@"C:\Program Files (x86)\WiX Toolset v3.6\bin\light.exe" -ext WixNetFxExtension -ext wixUtilExtension -out ..\Deploy\Enterprise\NDODevEdition21.msi NDODevEdition21.wixobj
-pause
