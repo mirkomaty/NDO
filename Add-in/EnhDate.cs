f664f981b121f74e9f4b04ab8af4b90c2ca127fd
mirko <mirko@e088091c-91c0-7b4b-9d36-2eafbd79b41f>
2014-01-27 09:24:11 +0000

    Removing EnhDate.cs from the repository because it will be generated
    git-svn-id: http://www.netdataobjects.de:81/svn/NDO20/trunk@155 e088091c-91c0-7b4b-9d36-2eafbd79b41f

--- Add-in/EnhDate.cs
+++
@@ -1,7 +0,0 @@
-namespace NDOEnhancer
-{
-	public class EnhDate
-	{
-		public const string String = "NDO: 10/20/2010 (V. 2.1 r. 87)";
-	}
-}
--- NDOEnhancer/EnhDate.cs
+++
@@ -1,7 +0,0 @@
-namespace NDOEnhancer
-{
-	public class EnhDate
-	{
-		public const string String = "NDO: 11/08/2012 (V. 2.1 r. 128)";
-	}
-}
