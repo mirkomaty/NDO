6c7fecead6a157dfc044635dbb83ed0fc88e9819
mirko <mirko.matytschak@formfakten.de>
2016-03-21 13:41:05 +0100

    Remove the old makefile

--- Make/BuildEnt.bat
+++
@@ -1,6 +0,0 @@
-@echo Start Visual Studio!
-@call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
-@SET DEVPATH="C:\Program Files (x86)\Microsoft Visual Studio 2010\Common7\IDE\PublicAssemblies"
-@SET V=ENT
-"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\nmake.exe" -f c:\projekte\ndo\make\makefileent.txt
-pause
