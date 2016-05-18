826f634e2a84ba4fbed566b04850e8e0e0f68d1e
mirko <mirko.matytschak@formfakten.de>
2016-05-18 14:09:32 +0200

    Neue Assembly-Version, damit nicht ältere Versionen der BuildTask im VS verwendet werden.

--- NDOEnhancer.BuildTask/NDOEnhancer.BuildTask/Properties/AssemblyInfo.cs
+++ NDOEnhancer.BuildTask/NDOEnhancer.BuildTask/Properties/AssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle( "NDOEnhancer.BuildTask" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "Microsoft" )]
+[assembly: AssemblyCompany( "Mirko Matytschak" )]
 [assembly: AssemblyProduct( "NDOEnhancer.BuildTask" )]
-[assembly: AssemblyCopyright( "Copyright © Microsoft 2013" )]
+[assembly: AssemblyCopyright( "Copyright © Mirko Matytschak 2016" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -54,5 +54,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "3.0.0.6" )]
+[assembly: AssemblyFileVersion( "3.0.0.6" )]
