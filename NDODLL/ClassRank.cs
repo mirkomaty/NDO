288722f588b2fcb58a9b5d89cb19a88a85ddd461
Mirko <mirko.matytschak@formfakten.de>
2016-07-07 14:13:24 +0200

    Extract building of the update rank in an own class

---
+++ NDODLL/ClassRank.cs
@@ -0,0 +1,153 @@
+﻿using NDO.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NDO
+{
+	public class ClassRank
+	{
+		Dictionary<Type, int> updateOrder;
+		private List<Type> updateList = new List<Type>();
+		private List<Type> clientGeneratedList = new List<Type>();
+		private List<Class> updateSearchedFor = new List<Class>();
+		IEnumerable<Class> classes;
+
+		public Dictionary<Type, int> BuildUpdateRank(IEnumerable<Class> classes)
+		{
+			this.classes = classes;
+			this.updateOrder = new Dictionary<Type, int>();
+			foreach (Class c in classes)
+			{
+				BuildUpdateDependency( c );
+				/*				Console.WriteLine("Class {0} Subclasses:", c.FullName);
+								foreach(Class s in c.Subclasses) {
+									Console.WriteLine("  - Class {0}", s.FullName);					
+								}*/
+			}
+
+			//Console.WriteLine("Dependencies:");
+			// The list will copied into a hashtable together with the
+			// rank of the type in the list. The rank is reverted.
+			// Lower rank values have more priority in the update order.
+			int end = updateList.Count - 1;
+			int i;
+			for (i = 0; i <= end; i++)
+			{
+				updateOrder.Add( (Type) updateList[i], end - i );
+			}
+
+			int rank = i;
+
+			// Now add the types using client generated oids. These don't need a 
+			// high rank.
+			int end2 = clientGeneratedList.Count - 1;
+			for (i = 0; i <= end2; i++)
+			{
+				updateOrder.Add( (Type) clientGeneratedList[i], rank++ );
+			}
+
+			return this.updateOrder;
+		}
+
+		/// <summary>
+		/// Builds update ranks.
+		/// </summary>
+		/// <param name="c">the class that should be inserted in the update list</param>
+		private void BuildUpdateDependency( Class c )
+		{
+			//Console.WriteLine("Build dependency for " + c.FullName);
+			if (c.SystemType == null)
+			{
+				throw new NDOException( 11, "Type.GetType for the type name " + c.FullName + " failed; check your mapping File." );
+			}
+
+			//Debug.WriteLine("BuildUpdateDependency " + c.FullName);
+
+			// Avoid endless recursion
+			if (updateSearchedFor.Contains( c ))
+				return;
+			updateSearchedFor.Add( c );
+			// Can be stored at last, because the key is client generated
+			if (!c.Oid.HasAutoincrementedColumn)
+			{
+				clientGeneratedList.Add( c.SystemType );
+				//updateList.Add( c.SystemType );
+				return;
+			}
+			int minIndex = int.MinValue;
+			foreach (Relation r in c.Relations)
+			{
+				if (r.MappingTable != null)  // Foreign Keys will be managed by NDO
+					continue;
+				if (r.Multiplicity == RelationMultiplicity.Element)
+				{
+					// Here we have two keys in each table. There is some probability, that the owner in a composite is yet saved.
+					if (!(r.Bidirectional && r.ForeignRelation.Multiplicity == RelationMultiplicity.Element && r.Composition))
+						continue;
+				}
+				if (c.FullName == r.ReferencedTypeName)
+					continue;
+				foreach (Class sc in r.ReferencedSubClasses)
+				{
+					// The foreign key is in the table of the referenced class.
+					// So the type has to be placed first in the update list.
+					// This will result in a higher rank value, which means, that 
+					// the type will be updated later than the current type.
+					BuildUpdateDependency( sc );
+					// This is used for sanity check. The current type cannot be
+					// placed above the referenced type. This may happen in certain scenarios 
+					// of circular references.
+					minIndex = Math.Max( minIndex, updateList.IndexOf( sc.SystemType ) );
+				}
+			}
+
+			foreach (Class c2 in classes)
+			{
+				if (c2.FullName == c.FullName)
+					continue;
+
+				foreach (Relation r in c2.Relations)
+				{
+					// Suche alle Relationen, die sich auf unsere Klasse beziehen
+					bool found = false;
+					foreach (Class cl in r.ReferencedSubClasses)
+					{
+						if (cl.FullName == c.FullName)
+						{
+							found = true;
+							break;
+						}
+					}
+					if (!found)
+						continue;
+					if (r.Multiplicity != RelationMultiplicity.Element)
+						continue;
+					// c2 has an element relation to c. This means, that c2 holds the foreign key
+					// and thus must appear first in the updateList.
+					// This will result in a higher rank value, which means, that 
+					// the type will be updated later than the current type.
+					BuildUpdateDependency( c2 );
+					// This is used for sanity check. The current type cannot be
+					// placed above the referenced type. This may happen in certain scenarios 
+					// of circular references.
+					minIndex = Math.Max( minIndex, updateList.IndexOf( c2.SystemType ) );
+					break;
+				}
+			}
+
+			int ix = updateList.IndexOf( c.SystemType );
+			if (ix >= 0 && ix < minIndex)
+			{
+				throw new NDOException( 12, "Cannot construct update dependencies for class " + c.SystemType + ". Try to use client generated primary keys for this type or for the related types." );
+			}
+			else if (!updateList.Contains( c.SystemType ) && !clientGeneratedList.Contains( c.SystemType ))
+			{
+				updateList.Add( c.SystemType );
+			}
+			//Console.WriteLine("End build dependency for " + c.FullName);
+		}
+
+	}
+}
--- NDODLL/Mappings.cs
+++ NDODLL/Mappings.cs
@@ -38,9 +38,6 @@ namespace NDO
 	internal class Mappings : NDOMapping
 	{
 		private Hashtable persistenceHandler = new Hashtable();
-		private ArrayList updateList = new ArrayList();
-		private ArrayList clientGeneratedList = new ArrayList();
-		private ArrayList updateSearchedFor = new ArrayList();
 		private Hashtable updateOrder = new Hashtable();
 		private DataSet ds;
 		ILogAdapter logAdapter;
@@ -66,7 +63,7 @@ namespace NDO
 		{
 			this.defaultHandlerType = defaultHandlerType;
 			InitClassFields();
-			BuildUpdateDependencies();
+			new ClassRank().BuildUpdateRank( Classes );
 		}
 
 		private void InitClassFields()
@@ -306,43 +303,6 @@ namespace NDO
 			return relations;
 		}
 
-		/// <summary>
-		/// Build dependency list for DataAdapter updates. Classes which are in front of the list must be
-		/// updated first. Note that the order is reversed for creation and deletion of objects.
-		/// This is handled transparently by the PM.
-		/// </summary>
-		private void BuildUpdateDependencies()
-		{
-			foreach ( Class c in Classes )
-			{
-				BuildUpdateDependency( c );
-				/*				Console.WriteLine("Class {0} Subclasses:", c.FullName);
-								foreach(Class s in c.Subclasses) {
-									Console.WriteLine("  - Class {0}", s.FullName);					
-								}*/
-			}
-
-			//Console.WriteLine("Dependencies:");
-			// The list will copied into a hashtable together with the
-			// rank of the type in the list. The rank is reverted.
-			// Lower rank values have more priority in the update order.
-			int end = updateList.Count - 1;
-			int i;
-			for ( i = 0; i <= end; i++ )
-			{
-				updateOrder.Add( (Type) updateList[i], end - i );
-			}
-
-			int rank = i;
-
-			// Now add the types using client generated oids. These don't need a 
-			// high rank.
-			int end2 = clientGeneratedList.Count - 1;
-			for ( i = 0; i <= end2; i++ )
-			{
-				updateOrder.Add( (Type) clientGeneratedList[i], rank++ );
-			}
-		}
 
 		/// <summary>
 		/// Returns the update order of a class. Higher values means that the class should be updated after
@@ -355,105 +315,6 @@ namespace NDO
 			if ( t.IsGenericType && !t.IsGenericTypeDefinition )
 				t = t.GetGenericTypeDefinition();
 			return (int) updateOrder[t];
-		}
-
-
-		/// <summary>
-		/// Used internally to build the list of update dependencies.
-		/// </summary>
-		/// <param name="c">the class that should be inserted in the update list</param>
-		private void BuildUpdateDependency( Class c )
-		{
-			//Console.WriteLine("Build dependency for " + c.FullName);
-			if ( c.SystemType == null )
-			{
-				throw new NDOException( 11, "Type.GetType for the type name " + c.FullName + " failed; check your mapping File." );
-			}
-
-			//Debug.WriteLine("BuildUpdateDependency " + c.FullName);
-
-			// Avoid endless recursion
-			if ( updateSearchedFor.Contains( c ) )
-				return;
-			updateSearchedFor.Add( c );
-			// Can be stored at last, because the key is client generated
-			if ( !c.Oid.HasAutoincrementedColumn )
-			{
-				clientGeneratedList.Add( c.SystemType );
-				//updateList.Add( c.SystemType );
-				return;
-			}
-			int minIndex = int.MinValue;
-			foreach ( Relation r in c.Relations )
-			{
-				if ( r.MappingTable != null )  // Foreign Keys will be managed by NDO
-					continue;
-				if ( r.Multiplicity == RelationMultiplicity.Element )
-				{
-					// Here we have two keys in each table. There is some probability, that the owner in a composite is yet saved.
-					if ( !(r.Bidirectional && r.ForeignRelation.Multiplicity == RelationMultiplicity.Element && r.Composition) )
-						continue;
-				}
-				if ( c.FullName == r.ReferencedTypeName )
-					continue;
-				foreach ( Class sc in r.ReferencedSubClasses )
-				{
-					// The foreign key is in the table of the referenced class.
-					// So the type has to be placed first in the update list.
-					// This will result in a higher rank value, which means, that 
-					// the type will be updated later than the current type.
-					BuildUpdateDependency( sc );
-					// This is used for sanity check. The current type cannot be
-					// placed above the referenced type. This may happen in certain scenarios 
-					// of circular references.
-					minIndex = Math.Max( minIndex, updateList.IndexOf( sc.SystemType ) );
-				}
-			}
-
-			foreach ( Class c2 in Classes )
-			{
-				if ( c2.FullName == c.FullName )
-					continue;
-
-				foreach ( Relation r in c2.Relations )
-				{
-					// Suche alle Relationen, die sich auf unsere Klasse beziehen
-					bool found = false;
-					foreach ( Class cl in r.ReferencedSubClasses )
-					{
-						if ( cl.FullName == c.FullName )
-						{
-							found = true;
-							break;
-						}
-					}
-					if ( !found )
-						continue;
-					if ( r.Multiplicity != RelationMultiplicity.Element )
-						continue;
-					// c2 has an element relation to c. This means, that c2 holds the foreign key
-					// and thus must appear first in the updateList.
-					// This will result in a higher rank value, which means, that 
-					// the type will be updated later than the current type.
-					BuildUpdateDependency( c2 );
-					// This is used for sanity check. The current type cannot be
-					// placed above the referenced type. This may happen in certain scenarios 
-					// of circular references.
-					minIndex = Math.Max( minIndex, updateList.IndexOf( c2.SystemType ) );
-					break;
-				}
-			}
-
-			int ix = updateList.IndexOf( c.SystemType );
-			if ( ix >= 0 && ix < minIndex )
-			{
-				throw new NDOException( 12, "Cannot construct update dependencies for class " + c.SystemType + ". Try to use client generated primary keys for this type or for the related types." );
-			}
-			else if ( !updateList.Contains( c.SystemType ) && !clientGeneratedList.Contains(c.SystemType) )
-			{
-				updateList.Add( c.SystemType );
-			}
-			//Console.WriteLine("End build dependency for " + c.FullName);
 		}
 	}
 }
