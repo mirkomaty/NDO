1819f7a3b3315550ac0c97186ffc894eb30e08f1
Mirko <mirko.matytschak@formfakten.de>
2018-05-28 16:51:45 +0200

    Add missing file

---
+++ NDOEnhancer/Enhancer/Corlib.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NDOEnhancer
+{
+	public enum FxType
+	{
+		NetFx,
+		Standard2
+	}
+
+	class Corlib
+	{
+		public static FxType FxType { get; set; }
+
+		public static string Name
+		{
+			get
+			{
+				return FxType == FxType.NetFx ? "[mscorlib]" : "[netstandard]";
+			}
+		}
+	}
+}
