70502829f15b65221650289d49523caa1997413a
Mirko <mirko.matytschak@formfakten.de>
2018-07-16 16:48:42 +0200

    Install NUnit as Nuget package Backwards Compatibility with the ExpectedExceptionAttribute

---
+++ TestEnhancerVersion4/NdoUnitTests/ExpectedExceptionAttribut.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NdoUnitTests
+{
+    public class ExpectedExceptionAttribute : Attribute
+    {
+        public Type ExpectedException { get; set; }
+        public ExpectedExceptionAttribute(Type expectedExceptionType)
+        {
+            ExpectedException = expectedExceptionType;
+        }
+
+    }
+}
