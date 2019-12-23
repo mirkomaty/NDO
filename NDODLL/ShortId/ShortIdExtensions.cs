01d7b3d5ba26511970b082ee3fad61a1b4a0fee3
Mirko <mirko.matytschak@formfakten.de>
2019-12-23 14:45:38 +0100

    #2133: Develop a Json Formatter which can serialize/deserialize ObjectContainers and ChangeSetContainers

--- NDODLL/ShortId/ShortIdExtensions.cs
+++ NDODLL/ShortId/ShortIdExtensions.cs
@@ -104,7 +104,7 @@ namespace NDO.ShortId
 		/// <returns></returns>
 		public static string ShortId(this IPersistentObject pc)
 		{
-			return pc.NDOObjectId.ToShortId();
+			return pc.NDOObjectId?.ToShortId();
 		}
 
 		/// <summary>
---
+++ NdoJsonFormatter/NdoJsonFormatter/NdoJsonFormatter.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using NDO;
+using NDO.ShortId;
+using NDO.Mapping;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NDO.JsonFormatter
+{
+	public class NdoJsonFormatter : IFormatter
+	{
+		public ISurrogateSelector SurrogateSelector { get; set; }
+		public SerializationBinder Binder { get; set; }
+		public StreamingContext Context { get; set; }
+
+		PersistenceManager pm;
+
+
+		public NdoJsonFormatter()
+		{
+			this.pm = null;
+		}
+
+		public NdoJsonFormatter( PersistenceManager pm )
+		{
+			this.pm = pm;
+		}
+
+		public IPersistenceCapable DeserializeObject( JToken jobj )
+		{
+			var shortId = (string)jobj["_oid"];
+
+			if (shortId != null)
+			{
+				var pc = this.pm.FindObject(shortId);
+				pc.NDOStateManager = null;  // Detach object
+				pc.FromJToken( jobj );
+				return pc;
+			}
+
+			return null;
+		}
+
+		public IPersistenceCapable DeserializeHollowObject(JToken jobj)
+		{
+			var shortId = (string)jobj["_oid"];
+
+			if (shortId != null)
+			{
+				var pc = this.pm.FindObject(shortId);
+				pc.NDOStateManager = null;  // Detach object
+				return pc;
+			}
+
+			return null;
+		}
+
+		public RelationChangeRecord DeserializeRelationChangeRecord(JToken jobj)
+		{
+			var parent = this.pm.FindObject((string)jobj["parent"]["_oid"]);
+			parent.NDOStateManager = null;  // Detach object
+			var child = this.pm.FindObject((string)jobj["child"]["_oid"]);
+			child.NDOStateManager = null;  // Detach object
+
+			var relationName = (string)jobj["relationName"];
+			var isAdded = (bool)jobj["isAdded"];
+			return new RelationChangeRecord(parent, child, relationName, isAdded);
+		}
+
+		void FixRelations( JToken jObj, IPersistenceCapable e, List<IPersistenceCapable> rootObjects, List<IPersistenceCapable> additionalObjects )
+		{
+			var t = e.GetType();
+			FieldMap fm = new FieldMap(t);
+			var mc = Metaclasses.GetClass(t);
+			foreach (var fi in fm.Relations)
+			{
+				var token = jObj[fi.Name];
+				if (token == null)
+					continue;
+				bool isArray = typeof(IList).IsAssignableFrom( fi.FieldType );
+				if (token is JArray jarray)
+				{
+					if (isArray)
+					{
+						IList container = (IList)fi.GetValue(e);						
+						if (container == null)
+							throw new NDOException( 20002, $"Container object of relation {t.Name}.{fi.Name} is not initialized. Please initialize the field in your class constructor." );
+
+						container.Clear();
+						foreach (var relJObj in jarray)
+						{
+							container.Add( DeserializeObject( relJObj ) );
+						}
+					}
+				}
+				else
+				{
+					if (!isArray)
+					{
+						if (token.Type == JTokenType.Null)
+							fi.SetValue( e, null );
+						else
+							fi.SetValue( e, DeserializeObject( token ) );
+					}
+				}
+			}
+		}
+
+		IList DeserializeChangeSetContainer(JToken rootArray)
+		{
+			// 0: AddedObjects = new List<IPersistenceCapable>();
+			// 1: DeletedObjects = new List<ObjectId>();
+			// 2: ChangedObjects = new List<IPersistenceCapable>();
+			// 3: RelationChanges = new List<RelationChangeRecord>();
+
+			ArrayList arrayList = new ArrayList(new object[4]);
+
+			for (int i = 0; i < 4; i++)
+			{
+				var partialArray = rootArray[i];
+				if (i == 0 || i == 2)
+				{
+					var partialList = new List<IPersistenceCapable>();
+					arrayList[i] = partialList;
+					foreach (var item in partialArray)
+					{
+						partialList.Add(DeserializeObject(item));
+					}
+				}
+				if (i == 1)
+				{
+					var partialList = new List<IPersistenceCapable>();
+					arrayList[i] = partialList;
+					foreach (var item in partialArray)
+					{
+						partialList.Add(DeserializeHollowObject(item));
+					}
+				}
+				if (i == 3)
+				{
+					var partialList = new List<RelationChangeRecord>();
+					arrayList[i] = partialList;
+					foreach (var item in partialArray)
+					{
+						partialList.Add(DeserializeRelationChangeRecord(item));
+					}
+				}
+			}
+
+			return arrayList;
+		}
+
+		object DeserializeRootArray( JToken rootArray )
+		{
+			var rootObjectsToken = (JArray)rootArray["rootObjects"];
+			var additionalObjectsToken = (JArray)rootArray["additionalObjects"];
+			if (rootObjectsToken.Count >= 4 && rootObjectsToken[0] is JArray)
+				return DeserializeChangeSetContainer(rootObjectsToken);
+
+			List<IPersistenceCapable> rootObjects = new List<IPersistenceCapable>();
+			List<IPersistenceCapable> additionalObjects = new List<IPersistenceCapable>();
+
+			foreach (var jObj in rootObjectsToken)
+			{
+				IPersistenceCapable e = DeserializeObject(jObj);
+				if (e != null)
+					rootObjects.Add( e );
+			}
+
+			foreach (var jObj in additionalObjectsToken)
+			{
+				IPersistenceCapable e = DeserializeObject(jObj);
+				if (e != null)
+					additionalObjects.Add( e );
+			}
+
+			foreach (var jObj in rootObjectsToken)
+			{
+				var shortId = (string)jObj["_oid"];
+				IPersistenceCapable e = rootObjects.First(o=>((IPersistenceCapable)o).ShortId() == shortId);
+				FixRelations( jObj, e, rootObjects, additionalObjects );
+			}
+
+			foreach (var jObj in additionalObjectsToken)
+			{
+				var shortId = (string)jObj["_oid"];
+				IPersistenceCapable e = additionalObjects.First(o=>((IPersistenceCapable)o).ShortId() == shortId);
+				FixRelations( jObj, e, rootObjects, additionalObjects );
+			}
+
+			return new ArrayList( rootObjects );
+		}
+
+		class Metaclasses
+		{
+			private static Hashtable theClasses = new Hashtable();
+
+			internal static IMetaClass GetClass( Type t )
+			{
+				if (t.IsGenericTypeDefinition)
+					return null;
+
+				IMetaClass mc;
+
+				lock (theClasses)
+				{
+					if (null == ( mc = (IMetaClass) theClasses[t] ))
+					{
+						Type mcType = t.GetNestedType("MetaClass", BindingFlags.NonPublic | BindingFlags.Public);
+						if (null == mcType)
+							throw new NDOException( 13, "Missing nested class 'MetaClass' for type '" + t.Name + "'; the type doesn't seem to be enhanced." );
+						Type t2 = mcType;
+						if (t2.IsGenericTypeDefinition)
+							t2 = t2.MakeGenericType( t.GetGenericArguments() );
+						mc = (IMetaClass) Activator.CreateInstance( t2 );
+						theClasses.Add( t, mc );
+					}
+				}
+
+				return mc;
+			}
+		}
+
+		public object Deserialize( Stream serializationStream )
+		{
+			if (this.pm == null)
+				throw new NDOException( 20001, "PersistenceManager is not initialized. Provide a PersistenceManager in the formatter constructor." );
+
+			JsonSerializer serializer = new JsonSerializer();
+			TextReader textReader = new StreamReader( serializationStream );
+			var rootObject = (JToken)serializer.Deserialize(textReader, typeof(JToken));
+			var result = DeserializeRootArray( rootObject );
+			this.pm.UnloadCache();
+			return result;
+		}
+
+		IDictionary<string, object> MakeDict( IPersistenceCapable pc )
+		{
+			var dict = pc.ToDictionary(pm);
+			var shortId = ((IPersistenceCapable)pc).ShortId();
+			var t = pc.GetType();
+			FieldMap fm = new FieldMap(t);
+			var mc = Metaclasses.GetClass(t);
+			foreach (var fi in fm.Relations)
+			{
+				if (( (IPersistenceCapable) pc ).NDOGetLoadState( mc.GetRelationOrdinal( fi.Name ) ))
+				{
+					object relationObj = fi.GetValue(pc);
+					if (relationObj is IList list)
+					{
+						List<object> dictList = new List<object>();
+						foreach (IPersistenceCapable relObject in list)
+						{
+							shortId = ( (IPersistenceCapable) relObject ).ShortId();
+							dictList.Add( new { _oid = shortId } );
+						}
+						dict.Add( fi.Name, dictList );
+					}
+					else
+					{
+						if (relationObj == null)
+						{
+							dict.Add( fi.Name, null );
+						}
+						else
+						{
+							IPersistenceCapable relIPersistenceCapable = (IPersistenceCapable) relationObj;
+							shortId = ( (IPersistenceCapable) relIPersistenceCapable ).ShortId();
+							dict.Add( fi.Name, new { _oid = shortId } );
+						}
+					}
+				}
+			}
+
+			return dict;
+		}
+
+		void InitializePm( object graph )
+		{
+			IPersistenceCapable pc;
+			if (graph is IList list)
+			{
+				if (list.Count == 0)
+					return;
+				pc = (IPersistenceCapable) list[0];
+			}
+			else
+			{
+				pc = (IPersistenceCapable) graph;
+			}
+
+			this.pm = (PersistenceManager) ( pc.NDOStateManager.PersistenceManager );
+		}
+
+		void RecursivelyAddAdditionalObjects( IPersistenceCapable e, List<IPersistenceCapable> rootObjects, List<IPersistenceCapable> additionalObjects )
+		{
+			var t = e.GetType();
+			FieldMap fm = new FieldMap(t);
+			var mc = Metaclasses.GetClass(t);
+			foreach (var fi in fm.Relations)
+			{
+				if (( (IPersistenceCapable) e ).NDOGetLoadState( mc.GetRelationOrdinal( fi.Name ) ))
+				{
+					object relationObj = fi.GetValue(e);
+					if (relationObj is IList list)
+					{
+						List<object> dictList = new List<object>();
+						foreach (IPersistenceCapable relIPersistenceCapable in list)
+						{
+							if (!rootObjects.Contains( relIPersistenceCapable ) && !additionalObjects.Contains( relIPersistenceCapable ))
+							{
+								additionalObjects.Add( relIPersistenceCapable );
+								RecursivelyAddAdditionalObjects( relIPersistenceCapable, rootObjects, additionalObjects );
+							}
+						}
+					}
+					else
+					{
+						if (relationObj != null)
+						{
+							IPersistenceCapable relIPersistenceCapable = (IPersistenceCapable) relationObj;
+							if (!rootObjects.Contains( relIPersistenceCapable ) && !additionalObjects.Contains( relIPersistenceCapable ))
+							{
+								additionalObjects.Add( relIPersistenceCapable );
+								RecursivelyAddAdditionalObjects( relIPersistenceCapable, rootObjects, additionalObjects );
+							}
+						}
+					}
+				}
+			}
+		}
+
+		object MapRelationChangeRecord(RelationChangeRecord rcr)
+		{
+			return new
+			{
+				parent = new { _oid = rcr.Parent.NDOObjectId.ToShortId() },
+				child  = new { _oid = rcr.Child.NDOObjectId.ToShortId() },
+				isAdded = rcr.IsAdded,
+				relationName = rcr.RelationName,
+				_oid = "RelationChangeRecord"
+			};
+		}
+
+		void SerializeChangeSet(Stream serializationStream, IList graph)
+		{
+			// A ChangeSetContainer consists of 4 lists of deleted, added, changed objects, and the relation changes.
+			// AddedObjects = new List<IPersistenceCapable>();
+			// DeletedObjects = new List<ObjectId>();
+			// ChangedObjects = new List<IPersistenceCapable>();
+			// RelationChanges = new List<RelationChangeRecord>();
+			List<List<object>> resultObjects = new List<List<object>>(graph.Count);
+			foreach (IList list in graph)
+			{
+				List<object> partialResult = new List<object>();
+				resultObjects.Add(partialResult);
+				foreach (var item in list)
+				{
+					if (item is IPersistenceCapable pc)
+						partialResult.Add(MakeDict(pc));
+					else if (item is ObjectId oid)
+						partialResult.Add(new { _oid = oid.ToShortId() });
+					else if (item is RelationChangeRecord rcr)
+						partialResult.Add(MapRelationChangeRecord(rcr));
+					else
+						throw new NDOException(20003, $"NDOJsonFormatter: unknown element of type {item.GetType().FullName} in ChangeSetContainer.");
+				}
+			}
+
+			var json = JsonConvert.SerializeObject(new { rootObjects = resultObjects, additionalObjects = new object[] { } });
+			var byteArray = Encoding.UTF8.GetBytes(json);
+			serializationStream.Write(byteArray, 0, byteArray.Length);
+		}
+
+		public void Serialize( Stream serializationStream, object graph )
+		{
+			string json = null;
+			if (this.pm == null)
+				InitializePm( graph );
+			List<object> rootObjects = new List<object>();
+			List<object> additionalObjects = new List<object>();
+			List<IPersistenceCapable> rootObjectList = new List<IPersistenceCapable>();
+			List<IPersistenceCapable> additionalObjectList = new List<IPersistenceCapable>();
+
+			IList list = graph as IList;
+			if (list != null)
+			{
+				if (list.Count > 0 && list[0] is IList)
+				{
+					// Change Set
+					SerializeChangeSet(serializationStream, list);
+					return;
+				}
+				foreach (IPersistenceCapable e in list)
+				{
+					rootObjectList.Add( e );
+				}
+			}
+			else if (graph is IPersistenceCapable e)
+			{
+				rootObjectList.Add( e );
+			}
+
+			foreach (var e in rootObjectList)
+			{
+				RecursivelyAddAdditionalObjects( e, rootObjectList, additionalObjectList );
+			}
+
+			foreach (var e in rootObjectList)
+			{
+				rootObjects.Add( MakeDict( e ) );
+			}
+
+			foreach (var e in additionalObjectList)
+			{
+				additionalObjects.Add( MakeDict( e ) );
+			}
+
+			json = JsonConvert.SerializeObject( new { rootObjects, additionalObjects } );
+			var byteArray = Encoding.UTF8.GetBytes(json);
+			serializationStream.Write( byteArray, 0, byteArray.Length );
+		}
+	}
+}
