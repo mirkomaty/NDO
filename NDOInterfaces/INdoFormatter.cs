ac70ebb562c6f05291ac928f5a5d61870b82f602
Mirko <mirko.matytschak@formfakten.de>
2024-04-14 12:20:04 +0200

    Support .NET 8 Replace IFormatter with INdoFormatter

---
+++ NDOInterfaces/INdoFormatter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NDOInterfaces
+{
+	/// <summary>
+	/// Replacement for the obsolete IFormatter interface.
+	/// </summary>
+	public interface INdoFormatter
+	{
+		/// <summary>
+		/// Deserializes an object or object graph from a stream
+		/// </summary>
+		/// <param name="serializationStream"></param>
+		/// <returns></returns>
+		object Deserialize( Stream serializationStream );
+
+		/// <summary>
+		/// Serializes an object graph to a stream
+		/// </summary>
+		/// <param name="serializationStream"></param>
+		/// <param name="graph"></param>
+		void Serialize( Stream serializationStream, object graph );
+	}
+}
