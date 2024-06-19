38c26b039ef22d62b434badf892638dc724cc082
Mirko <mirkom@text-talk.de>
2024-06-19 14:36:39 +0200

    Add some QueryCache tests

--- IntegrationTests/IntegrationTests/QueryCacheTests.cs
+++ IntegrationTests/IntegrationTests/QueryCacheTests.cs
@@ -22,6 +22,13 @@ namespace NdoUnitTests
             pm.Save();
         }
 
+        [TearDown]
+        public void TearDown()
+        {
+            var pm = PmFactory.NewPersistenceManager();
+            pm.Objects<Mitarbeiter>().DeleteDirectly();
+        }
+
         [Test]
         public void TestSimpleQuery()
         {
@@ -30,12 +37,68 @@ namespace NdoUnitTests
             Assert.That( pm.QueryCache.Count, Is.EqualTo( 0 ) );
             pm.UseQueryCache = true;
             var result = pm.Objects<Mitarbeiter>().ResultTable;
+            Assert.That( result.Count == 1 );
             Assert.That( pm.QueryCache.Count, Is.EqualTo( 1 ) );
             pm.VerboseMode = true;
             var testAdapter = new TestLogAdapter();
             pm.LogAdapter = testAdapter;
             result = pm.Objects<Mitarbeiter>().ResultTable;
-            Assert.That( testAdapter.Text == "Getting results from QueryCache" );
+            Assert.That( result.Count == 1 );
+            Assert.That( testAdapter.Text.Trim() == "Getting results from QueryCache" );
+        }
+
+        [Test]
+        public void QueryCacheCanBeSwitchedOff()
+        {
+            CreateObject();
+            var pm = PmFactory.NewPersistenceManager();
+            Assert.That( pm.QueryCache.Count, Is.EqualTo( 0 ) );
+            pm.UseQueryCache = false;
+            var result = pm.Objects<Mitarbeiter>().ResultTable;
+            Assert.That( result.Count == 1 );
+            Assert.That( pm.QueryCache.Count, Is.EqualTo( 0 ) );
+            pm.VerboseMode = true;
+            var testAdapter = new TestLogAdapter();
+            pm.LogAdapter = testAdapter;
+            result = pm.Objects<Mitarbeiter>().ResultTable;
+            Assert.That( result.Count == 1 );
+            Assert.That( testAdapter.Text.IndexOf( "QueryCache" ) == -1 );
+        }
+
+        [Test]
+        public void DifferentQueryDoesntUseTheCache()
+        {
+            CreateObject();
+            var pm = PmFactory.NewPersistenceManager();
+            Assert.That( pm.QueryCache.Count, Is.EqualTo( 0 ) );
+            pm.UseQueryCache = true;
+            var result = pm.Objects<Mitarbeiter>().ResultTable;
+            Assert.That( result.Count == 1 );
+            Assert.That( pm.QueryCache.Count, Is.EqualTo( 1 ) );
+            pm.VerboseMode = true;
+            var testAdapter = new TestLogAdapter();
+            pm.LogAdapter = testAdapter;
+            result = pm.Objects<Mitarbeiter>().Where(m=>m.Vorname == "Mirko").ResultTable;
+            Assert.That( result.Count == 1 );
+            Assert.That( testAdapter.Text.IndexOf( "QueryCache" ) == -1 );
+        }
+
+        [Test]
+        public void TakeAndSkipDontUseTheSameCacheResult()
+        {
+            CreateObject();
+            var pm = PmFactory.NewPersistenceManager();
+            Assert.That( pm.QueryCache.Count, Is.EqualTo( 0 ) );
+            pm.UseQueryCache = true;
+            var result = pm.Objects<Mitarbeiter>().OrderBy(m=>m.Nachname).Skip(0).Take(1).ResultTable;
+            Assert.That( result.Count == 1 );
+            Assert.That( pm.QueryCache.Count, Is.EqualTo( 1 ) );
+            pm.VerboseMode = true;
+            var testAdapter = new TestLogAdapter();
+            pm.LogAdapter = testAdapter;
+            result = pm.Objects<Mitarbeiter>().OrderBy(m=>m.Nachname).Skip(0).Take(2).ResultTable;
+            Assert.That( result.Count == 1 );
+            Assert.That( testAdapter.Text.IndexOf( "QueryCache" ) == -1 );
         }
     }
 }
