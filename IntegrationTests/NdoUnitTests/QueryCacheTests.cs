7586755194d82b8ab47621dcd3cbdb4628ee317b
Mirko <mirkom@text-talk.de>
2024-06-19 14:08:02 +0200

    First attempt to use a QueryCache

---
+++ IntegrationTests/NdoUnitTests/QueryCacheTests.cs
@@ -0,0 +1,41 @@
+﻿using NDO.Logging;
+using NUnit.Framework;
+using Reisekosten.Personal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NdoUnitTests
+{
+    [TestFixture]
+    public class QueryCacheTests
+    {
+        void CreateObject()
+        {
+            var pm = PmFactory.NewPersistenceManager();
+            Mitarbeiter m = new Mitarbeiter();
+            m.Vorname = "Mirko";
+            m.Nachname = "Matytschak";
+            pm.MakePersistent( m );
+            pm.Save();
+        }
+
+        [Test]
+        public void TestSimpleQuery()
+        {
+            CreateObject();
+            var pm = PmFactory.NewPersistenceManager();
+            Assert.That( pm.QueryCache.Count, Is.EqualTo( 0 ) );
+            pm.UseQueryCache = true;
+            var result = pm.Objects<Mitarbeiter>().ResultTable;
+            Assert.That( pm.QueryCache.Count, Is.EqualTo( 1 ) );
+            pm.VerboseMode = true;
+            var testAdapter = new TestLogAdapter();
+            pm.LogAdapter = testAdapter;
+            result = pm.Objects<Mitarbeiter>().ResultTable;
+            Assert.That( testAdapter.Text == "Getting results from QueryCache" );
+        }
+    }
+}
